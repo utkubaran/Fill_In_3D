3866575bc1e9aa536a9e67ec5882aa18805bcef5
Mehmet Utku Baran <mbaran13@ku.edu.tr>
2021-03-02 17:29:43 +0300

    Cube Spawner and Pixel Arts are Added!
    CubeSpawner.cs and pixel arts are added. Positions of spawned cubes are adjusted according to parent game object.

---
+++ Fill_In_3D/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [Space]
+    [SerializeField]
+    private Sprite pixelArtSprite;
+
+    [Space]
+    [SerializeField]
+    private GameObject blockPrefab;
+
+    [Space]
+    [SerializeField]
+    private float offset = 0.32f;
+
+    Texture2D image;
+
+    Vector3 blockPos = Vector3.zero;
+
+    private void Awake()
+    {
+        image = pixelArtSprite.texture;
+
+        for (int x = 0; x < image.width; x++)
+        {
+            for (int y = 0; y < image.height; y++)
+            {
+                Color color = image.GetPixel(x, y);
+
+                if(color.a == 0)
+                {
+                    continue;
+                }
+
+                blockPos = new Vector3( offset * (x - image.width * 0.5f), offset * 0.5f, offset * (y - image.height * 0.5f));
+                GameObject blockObject = Instantiate(blockPrefab, transform);
+                blockObject.transform.localPosition = blockPos;
+                blockObject.GetComponent<Renderer>().material.color = color;
+            }
+        }
+    }
+}
---
+++ Fill_In_3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Vector3 startPos;
+    Vector3 deltaPos;
+    Vector3 direction;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            deltaPos = Input.mousePosition - startPos;
+
+            direction = new Vector3(deltaPos.x, 0f, deltaPos.y);
+            direction = direction.normalized * speed;
+
+            rb.velocity = direction;
+
+            Rotate();
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private void Rotate()
+    {
+        if (direction.magnitude >= 0.05f)
+        {
+            rb.freezeRotation = false;
+            rb.rotation = Quaternion.LookRotation(rb.velocity);
+        }
+        else
+        {
+            rb.freezeRotation = true;
+        }
+    }
+}
